c442bdad364ca736599514367366896a98235c19
Cornelia Hoppe <Ccc.hoppe@gmail.com>
2021-11-03 15:21:32 +0100

    gallery

--- script/app.js
+++ script/app.js
@@ -11,7 +11,7 @@ const images = [
       name: "pexels-chitokan-2087748.jpg",
     },
     {
-      name: "pexels-los-muertos-crew-7613568.png",
+      name: "pexels-los-muertos-crew-7613568.jpg",
     },
     {
       name: "pexels-los-muertos-crew-7772198.jpg",
@@ -27,19 +27,65 @@ const images = [
     },
   ];
 
-  const setMainImage = (src) => {
-    document.getElementById("image-container").setAttribute("src", src);
-    setActiveThumbNail();
-  };
+ 
+const setMainImage = (src) => {
+  document.getElementById("main-image").setAttribute("src", src);
+  setActiveThumbNail();
+};
 
-  window.addEventListener("load", () => {
-    document
-      .getElementById("image-container")
-      .setAttribute("src", `./img/${images[0].name}`);
-    document.getElementById("image-container").innerHTML = images
-      .map(
-        (img) =>
-          `<img src="./img/${img.name}" class="thumbnail" onclick="setMainImage(this.src)">`
-      )
-      .join("")});
+const setActiveThumbNail = () => {
+  const thumbs = document.getElementsByClassName("thumbnail");
+  for (let i = 0; i < thumbs.length; i++) {
+    if (thumbs[i].src === document.getElementById("main-image").src) {
+      thumbs[i].style.border = "2px solid red";
+    } else {
+      thumbs[i].style.border = "0px";
+    }
+  }
+};
 
+const prevImage = () => {
+  const thumbs = document.getElementsByClassName("thumbnail");
+  for (let i = 0; i < thumbs.length; i++) {
+    if (
+      thumbs[i].src === document.getElementById("main-image").src &&
+      i !== 0
+    ) {
+      document
+        .getElementById("main-image")
+        .setAttribute("src", thumbs[(i -= 1)].src);
+      setActiveThumbNail();
+    }
+  }
+};
+
+const nextImage = () => {
+  const thumbs = document.getElementsByClassName("thumbnail");
+  for (let i = 0; i < thumbs.length; i++) {
+    if (
+      thumbs[i].src === document.getElementById("main-image").src &&
+      i !== thumbs.length - 1
+    ) {
+      document
+        .getElementById("main-image")
+        .setAttribute("src", thumbs[(i += 1)].src);
+      setActiveThumbNail();
+    }
+  }
+};
+
+window.addEventListener("load", () => {
+  document
+    .getElementById("main-image")
+    .setAttribute("src", `./img/${images[0].name}`);
+  document.getElementById("thumbnails-wrapper").innerHTML = images
+    .map(
+      (img) =>
+        `<img src="./img/${img.name}" class="thumbnail" onclick="setMainImage(this.src)">`
+    )
+    .join("");
+
+  setActiveThumbNail();
+  document.getElementById("prev-btn").addEventListener("click", prevImage);
+  document.getElementById("next-btn").addEventListener("click", nextImage);
+});
