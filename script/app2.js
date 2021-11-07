6f5b76c82415871d8a67d4f35e2aa306dca31066
Daniel Häggström <danielhaggstrom@hotmail.se>
2021-11-07 13:29:31 +0100

    Finished the code for card-modals

--- script/app2.js
+++ script/app2.js
@@ -32,45 +32,77 @@ window.addEventListener("load", () => {
 });
 
 // Card modal code
-/*
-const closeServiceModal = () => {
-    document.getElementById("service-modal-wrapper").style.display = "none";
-};
-
-const openServiceModal = () => {
-    const cards = document.getElementsByClassName("service-card");
-    for (let i = 0; i < cards.length; i++) {
-        cards[i].addEventListener("click", () => {
-            document.getElementById("service-modal-wrapper").style.display = "flex";
-            for (let j = 0; j < services.length; j++) {
-                if (i === j) {
-                document.getElementById(
-                "service-modal-content"
-                ).innerHTML = `<h2>${services[j].name}</h2><p>${services[j].content}</p><img src="./media/img/${services[j].picture}">`;
-                }
-            }
-        });
-    }
-};
-
-window.addEventListener("load", openServiceModal);
-*/
-
 // Variables
+const cardModalWrapper = document.querySelector(".card-modal-wrapper");
+const cardModal = document.querySelector(".card-modal");
+const modalCross = document.querySelector("#modal-close");
+const imgContent = document.querySelector("#img-wrapper");
+const infoContent = document.querySelector("#info-wrapper");
+// Staff information
 const staff = [
     {
         name: "Name1",
         role: "Chef",
-        info: `This is where the information for ${this.name}, ${this.role}`,
+        img: "s1.jpg",
+        info() { 
+            return `This is information about ${this.name}, the ${this.role}`;
+        },
     },
     {
         name: "Name2",
         role: "Head-Chef",
-        info: `This is where the information for ${this.name}, ${this.role}`,
+        img: "s2ny.jpeg",
+        info() { 
+            return `This is information about ${this.name}, the ${this.role}`;
+        },
     },
     {
         name: "Name3",
         role: "Chef",
-        info: `This is where the information for ${this.name}, ${this.role}`,
+        img: "s3ny.jpg",
+        info() { 
+            return `This is information about ${this.name}, the ${this.role}`;
+        },
     },
 ];
+
+// Opens and puts in the correct information to the card-modal
+const openCardModal = () => {
+    const readMore = document.querySelectorAll(".card-text p");
+    readMore.forEach((card, index) => {
+        card.addEventListener("click", () => {
+            cardModalWrapper.style.display = "flex";
+            for (let i = 0; i < staff.length; i++) {
+                if (index === i) {
+                    imgContent.innerHTML = `<img class="modal-img" src="./img/${staff[i].img}" />`;
+                    infoContent.innerHTML = `<h2>${
+                        staff[i].role
+                    }</h2><p>${staff[i].info()}</p>`;
+                }
+            }
+        });
+    });
+};
+
+// Closes the card modal when the cross is clicked
+const closeCardModal = (e) => {
+    if (e.target !== e.currentTarget) {
+        return;
+    }
+    if (cardModalWrapper.style.display == "flex") {
+        cardModalWrapper.style.display = "none";
+    } else {
+        cardModalWrapper.style.display = "flex";
+    }
+};
+
+
+// Making sure the website is loaded before any events are executed
+window.addEventListener("load", () => {
+    // Cross onClick event (closes it)
+    modalCross.addEventListener("click", closeCardModal);
+    cardModalWrapper.addEventListener("click", closeCardModal);
+    // Calling the function that opens the card-modal
+    openCardModal();
+});
+
